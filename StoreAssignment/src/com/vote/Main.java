87909a84858d0eee029dff125287c8e8d109e7bf
Swapnil Vhantale <103575498+spvhantale@users.noreply.github.com>
2023-10-15 11:17:06 +0530

    Updated Assignment

--- StoreAssignment/src/com/vote/Main.java
+++ StoreAssignment/src/com/vote/Main.java
@@ -5,37 +5,54 @@ import java.util.List;
 
 public class Main {
 
-	private static final double tshirtPrice = 50.00;
-	private static final double jeansPrice = 80.00;
-	private static final double jacketPrice = 100.00;
-	private static final double budget = 600;
+	private final double tshirtPrice = 50.00;
+	private final double jeansPrice = 80.00;
+	private final double jacketPrice = 100.00;
+	private final double budget = 600;
 
 	public static void main(String[] args) {
+
+		Main main = new Main();
+		List<String> list = main.calculate();
+		main.printResult(list);
+
+	}
+
+	public List<String> calculate() {
+
 		List<String> list = new ArrayList<>();
+
 		double minPrice = Math.min(tshirtPrice, Math.min(jeansPrice, jacketPrice));
 
 		int maxTshirt = (int) Math.floor(budget / tshirtPrice);
-		int maxJeans = (int) Math.floor( budget/jeansPrice);
+
+		int maxJeans = (int) Math.floor(budget / jeansPrice);
+
 		for (int i = 1; i <= maxTshirt; i++) {
 			for (int j = 1; j <= maxJeans; j++) {
 				int jackets = (int) Math.floor((budget - (i * tshirtPrice + j * jeansPrice)) / jacketPrice);
 				double totalPrice = (i * tshirtPrice) + (j * jeansPrice) + (jackets * jacketPrice);
 				double remainingBudget = budget - totalPrice;
 				if (jackets >= 1 && minPrice > remainingBudget) {
-					list.add("TShirt: " + i + ", Jeans: " + j + ", Jackets: " + jackets + ", Total Price:  "+totalPrice+", Remaining Budget: "
-							+ remainingBudget);
+					list.add("TShirt: " + i + ", Jeans: " + j + ", Jackets: " + jackets + ", Total Price:  "
+							+ totalPrice + ", Remaining Budget: " + remainingBudget);
 				}
 
 			}
 		}
+
+		return list;
+	}
+
+	public void printResult(List<String> list) {
+
+		System.out.println("The Combination of items: "+"\n");
 		
-		for(String data:list){
+		for (String data : list) {
 			System.out.println(data);
 		}
-		
-		System.out.println("Total Combination of Items is: "+list.size());
-		
 
+		System.out.println("\n"+"Total Combination of Items is: " + list.size());
 	}
 
 }
